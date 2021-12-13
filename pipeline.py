18f5386002b6ef5d02c6ae28153e9fde615aa55e
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-12-13 17:40:51 +0000

    use realpath

--- pipeline.py
+++ pipeline.py
@@ -73,7 +73,9 @@ from cgatcore import pipeline as P
 
 # Load parameters from config file, located in `./config/pipeline.yml`.
 print("%s/config/pipeline.yml" % os.path.dirname(__file__)[0])
-PARAMS = P.get_parameters(["%s/config/pipeline.yml" % os.path.dirname(__file__)[0]])
+PARAMS = P.get_parameters(
+    "%s/config/pipeline.yml" % os.path.dirname(os.path.realpath(__file__))[0]
+)
 print(PARAMS)
 
 ############
